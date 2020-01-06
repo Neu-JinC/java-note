aec2642c7335d54f35f163626a09238a6b334edf
JinC <13026590207@163.com>
2020-01-06 10:06:02 +0800

    code commit

---
+++ src/main/java/book/core/java/volume/II/chapter/one/io/InputStreamTest.java
@@ -0,0 +1,9 @@
+package book.core.java.volume.II.chapter.one.io;
+
+import java.io.InputStream;
+
+public class InputStreamTest {
+    public static void main(String[] args) {
+        InputStream inputStream;
+    }
+}
